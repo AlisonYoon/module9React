6e01a70c94962a432346636d79fed0b14c72d82a
AlisonYoon <politescarlett1@gmail.com>
2019-10-09 14:58:44 +0100

    inline event listeners in JSX

--- axolotls/src/components/Shiela/Shiela.js
+++ axolotls/src/components/Shiela/Shiela.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import Greg from '../Greg/Greg'
+import Greg from '../Greg/Greg';
+import './Shiela.css';
 
 class Shiela extends React.Component {
     render() {
+        let handleClick = () => {
+            document.querySelector('p').style.cssText = "color: #8F0ECE; font-size: 20px; font-weight: bold";
+            document.querySelector('.event-div').innerHTML = "Clicked";
+        };
+
         return (
             <div>
                 <p>Hello, I'm Shiela, a component with class</p>
-                <Greg />
-                <Greg />
+                <div className="event-div" onClick={handleClick}>Click me</div>
                 <Greg />
             </div>
         )
