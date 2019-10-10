1f2230dcda65104000fe7e2d566e1a9ac12adfbe
AlisonYoon <politescarlett1@gmail.com>
2019-10-10 13:19:13 +0100

    lifecycle of a component and methods

---
+++ lifecycle.js
@@ -0,0 +1,8 @@
+// Lifecycle
+// Exactly what it sounds like, the timeline from the creation to the destruction of a component
+// There are lifecycle methods that are automatically triggered along this timeline
+// that you can hook into(eg: just like any event listener in regular JS)
+// Component is rendered to the DOM first time, it's called "Mounting"
+//          -> componentDidMount()
+// Component is removed from the DOM is called "Unmounting"
+//          -> componentWillUnmount()
