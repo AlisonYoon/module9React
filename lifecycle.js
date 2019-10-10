a6d45f9c41de8c7e3e8cd5b1c6ce3bc815d9c441
AlisonYoon <politescarlett1@gmail.com>
2019-10-10 15:38:16 +0100

    when state is lifted up

--- lifecycle.js
+++ lifecycle.js
@@ -9,4 +9,88 @@
 // When a parent re-renders and passes new props to a child
 //          -> componentDidUpdate()    -> use this method in the children so that when a parent updates,
 //                                          children gets re-rendered as well with new props
+//              the first parameter of componentDidUpdate() gives you the access of the last state of the very previous render
 
+
+class ChildWithLifecycle extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            loggedIn: this.props.loggedIn
+        }
+    }
+
+    //lifecycle method that triggers when parent re-renders and passes new props to child
+    //triggers on every re-render
+    componentDidUpdate(prevProps) {
+        //remember to use an if statement to stop a never ending loop
+        if (this.props.loggedIn !== prevProps.loggedIn) {
+            this.setState(
+                {
+                    loggedIn: this.props.loggedIn
+                    //even if it keep assigns false(same false again and again), setState() will make it re-render
+                })
+        }
+    }
+
+    //this is just calling the method defined in the parent
+    doThing = () => {
+        this.props.clicker()
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{ this.state.loggedIn ? 'Logged In' : 'Logged Out' }</h1>
+                <button onClick={ this.doThing }>Log me out!</button>
+            </div>
+        )
+    }
+}
+
+class ChildWithoutLifecycle extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            loggedIn: this.props.loggedIn
+        }
+    }
+
+    //copy the componentDidUpdate func to show with lifecycle
+
+    render() {
+        return (
+            <div>
+                <h1>{ this.state.loggedIn ? 'Logged In' : 'Logged Out' }</h1>
+            </div>
+        )
+    }
+}
+
+class LoginDecider extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            loggedIn: true
+        }
+    }
+
+    //the function to be passed as a prop to children
+    handleClick = () => {
+        console.log('logging out')
+        this.setState({
+            loggedIn: false
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <ChildWithLifecycle loggedIn={ this.state.loggedIn } clicker={ this.handleClick }></ChildWithLifecycle>
+                <ChildWithoutLifecycle loggedIn={ this.state.loggedIn }></ChildWithoutLifecycle>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<LoginDecider />, document.getElementById('root'))
